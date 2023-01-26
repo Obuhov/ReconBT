5374a47ddf386e0306daf2d84ed25780b3555df5
Ivan Obuhov <65172086+Obuhov@users.noreply.github.com>
2023-01-27 04:40:07 +0500

    Added AuthorizationsToken 1) the Authorizations function of the user bot 2) message sending function
    The repository does not have an example of a basic environment variables file.

--- main.py
+++ main.py
@@ -31,3 +31,41 @@ class Bot:
 
         # Импорт id страницы
         self.main_user_id = os.getenv("USER_ID")
+
+    def do_auth(self):
+        # функция Authorizations бота-пользователя
+        """
+        :return:
+        """
+
+        token = os.getenv("ACCESS_TOKEN")
+
+        try:
+            self.vk_session = vk_api.VkApi(token=token)
+            return self.vk_session.get_api()
+        except Exception as error:
+            print(error)
+            return None
+
+    def send_message(self, receiver_user_id: str = None, message_text="тестовое сообщение"):
+        # функция отправки сообщения
+        """
+        :param receiver_user_id: id получателя сообщения
+        :param message_text: текст отправляемого сообщения
+        :return:
+        """
+
+        #
+        if not self.authorized:
+            print("CriticalError; check ACCESS_TOKEN?")
+            return
+
+        # нет id - значение из окружения переменных
+        if receiver_user_id is None:
+            receiver_user_id = self.main_user_id
+
+        try:
+            self.vk_api_access.messages.send(user_id=receiver_user_id, message=message_text, random_id=get_random_id())
+            print(f"Сообщение для ID {receiver_user_id} текст: {message_text}")
+        except Exception as error:
+            print(error)
